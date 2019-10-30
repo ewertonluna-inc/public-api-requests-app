cfb4653efe85c7b1af557c649f30565f01cb209a
Ewerton Luna <ewerton.al@gmail.com>
2019-10-30 15:44:46 -0300

    Add buttons functionalities

--- js/script.js
+++ js/script.js
@@ -30,9 +30,8 @@ async function getRandomUsers(numberOfUsers){
     const users = [];
 
     for (let i = 0; i < numberOfUsers; i++){
-        // Returns a json object
         const json = await getJSON(usersURL);
-        // json.results contains user information
+        // json.results contains the user information
         const user = json.results[0];
         users.push(user);
     }
@@ -46,29 +45,57 @@ async function getRandomUsers(numberOfUsers){
 
 /**
  * Generates HTML for the gallery
- * @param {Object} data User object
+ * @param {Object} user User object
  */
-function generateGaleryHTML(data){
-    const img = data.picture.medium;
-    const firstAndLastName = data.name.first + ' ' + data.name.last;
-    const email = data.email;
-    const cityAndState = data.location.city + ', ' + data.location.state;
-    const html = `
-        <div class="card">
-            <div class="card-img-container">
-                <img class="card-img" src="${img}" alt="profile picture">
+function generateGaleryHTML(users){
+    users.map( user => {
+        const img = user.picture.medium;
+        const firstAndLastName = user.name.first + ' ' + user.name.last;
+        const email = user.email;
+        const cityAndState = user.location.city + ', ' + user.location.state;
+        const html = `
+            <div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="${img}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${firstAndLastName}</h3>
+                    <p class="card-text">${email}</p>
+                    <p class="card-text cap">${cityAndState}</p>
+                </div>
             </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${firstAndLastName}</h3>
-                <p class="card-text">${email}</p>
-                <p class="card-text cap">${cityAndState}</p>
-            </div>
-        </div>
-    `;
-    document.getElementById('gallery').innerHTML += html;
+        `;
+        document.getElementById('gallery').innerHTML += html;
+    } );
+    
+    return users;
 }
 
-function generateModalHTML(data){
+function createModalContainer(html){
+    const modalContainer = document.createElement('div');
+    modalContainer.className = 'modal-container';
+    modalContainer.innerHTML = html;
+    document.querySelector('body').appendChild(modalContainer);
+}
+
+function getUserIndex(modalContainer){
+    const modalEmail = modalContainer.querySelector('p').textContent;
+    const cards = document.getElementsByClassName('card');
+    let userIndex = 0;
+
+    for (let card of cards){
+        const cardEmail = card.querySelector('p').textContent;
+        
+        if (cardEmail == modalEmail){
+            return userIndex;
+        }
+        userIndex++;
+    }
+
+    return null;
+}
+
+function generateModalHTML(data, users){
     const img = data.picture.medium;
     const firstAndLastName = data.name.first + ' ' + data.name.last;
     const email = data.email;
@@ -101,36 +128,67 @@ function generateModalHTML(data){
         </div>
     `;
     
-    const modalContainer = document.createElement('div');
-    modalContainer.className = 'modal-container';
-    modalContainer.innerHTML = html;
-    document.querySelector('body').appendChild(modalContainer);
+    createModalContainer(html);
+    const modalContainer = document.getElementsByClassName('modal-container')[0];
+    generateModalListeners(modalContainer, users);
+    
     return modalContainer;
 }
+
+function generateModalListeners(modalContainer, users){
+    const userIndex = getUserIndex(modalContainer);
+    const exitButton = modalContainer.querySelector('#modal-close-btn');
+    const prevButton = modalContainer.querySelector('#modal-prev');
+    const nextButton = modalContainer.querySelector('#modal-next');
+    exitButton.addEventListener('click', () => modalContainer.remove());
+    prevButton.addEventListener('click', () => {
+        modalContainer.remove();
+        if (userIndex != 0){
+            generateModalHTML(users[userIndex - 1], users);    
+        }
+        
+    });
+    nextButton.addEventListener('click', () => {
+        modalContainer.remove();
+        if (userIndex != 11){
+            generateModalHTML(users[userIndex + 1], users);    
+        }
+        
+    });
+}
+
 
 function generateCardsListeners(users){
     const cards = gallery.children;
       
     for (let card of cards){
-          card.addEventListener('click', () => {
-              let data;
-              const email = card.querySelector('p').textContent;
-              for (let user of users){
-                  if (user.email === email){
-                      data = user;
-                      break;
-                  }
-              }
+        
+        card.addEventListener('click', () => {
+            let data;
+            const email = card.querySelector('p').textContent;
               
-              const modalContainer = generateModalHTML(data);
-              // Maybe, create a function to add all event listeners to the modal div buttons
-              const exitButton = modalContainer.querySelector('#modal-close-btn');
-              exitButton.addEventListener('click', () => {
-                  modalContainer.remove();
-              })
-          });
+            for (let user of users){
+                if (user.email === email){
+                    data = user;
+                    break;
+                }
+            }
+              
+            generateModalHTML(data, users);
+        });
     }
+    return users;
 }
+
+function addButtonsListeners(modalContainer){
+    // continue
+    const exitButton = modalContainer.querySelector('#modal-close-btn');
+    const prevButton = modalContainer.querySelector('#modal-prev');
+    const nextButton = modalContainer.querySelector('#modal-next');
+    exitButton.addEventListener('click', () => modalContainer.remove());
+    prevButton.addEventListener('click', () => {});
+}
+
 
 // ------------------------------------------
 //  PAGE INITIAL SET UP
@@ -138,10 +196,7 @@ function generateCardsListeners(users){
 
 const promise = getRandomUsers(12);
 promise
-  .then( users => {
-      users.map( user => generateGaleryHTML(user) );
-      return users;
-  })
+  .then( generateGaleryHTML )
   .then( generateCardsListeners )
   .catch(err => console.log('Oops... An error occured:', err));
 
